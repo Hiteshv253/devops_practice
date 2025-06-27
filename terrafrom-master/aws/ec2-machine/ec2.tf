c4b1c0c88322f3b924d2fb820ff605291a23bc14
aws <aws@master-push-machin.localdomain>
2025-06-27 23:16:32 +0530

    pus

--- terrafrom-master/aws/ec2-machine/ec2.tf
+++ terrafrom-master/aws/ec2-machine/ec2.tf
@@ -2,43 +2,45 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0.0"
+      version = "~> 6.0"
     }
   }
+
+  required_version = ">= 1.5.0"
+
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
 
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  default     = "terraform-key.pem"
+}
 
-// To Generate Private Key
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-variable "key_name" {
-  description = "Name of the SSH key pair"
-}
-
-// Create Key Pair for Connecting EC2 via SSH
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-// Save PEM file locally
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = var.key_name
 }
 
-
-resource "aws_instance" "terrafrom-master" {
-  ami                    = "ami-09e6f87a47903347c"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.key_pair.key_name
+resource "aws_instance" "terraform-master" {
+  ami           = "ami-09e6f87a47903347c"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.key_name
 
   tags = {
-    Name = "terrafrom-master"
+    Name = "terraform-master"
   }
 
   root_block_device {
@@ -46,4 +48,3 @@ resource "aws_instance" "terrafrom-master" {
     volume_type = "gp2"
   }
 }
-
